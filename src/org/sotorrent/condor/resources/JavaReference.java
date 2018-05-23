47ee41f2160109ce67d08318e266f22d2a9d91a0
prmr <prmr@gmx.com>
2018-05-23 19:22:46 -0400

    #1 Add more classification...

--- src/org/sotorrent/condor/resources/JavaReference.java
+++ src/org/sotorrent/condor/resources/JavaReference.java
@@ -11,14 +11,16 @@ public class JavaReference extends DeveloperResource
         super("oracle.com", "sun.com");
 
         createResourcePattern(new String[]{
-                // TODO: add patterns for java.sun.com links
                 "^https?://docs\\.oracle\\.com/java(s|e)e/tutorial/.*",
                 "^https?://docs\\.oracle\\.com/java(s|e)e/[\\d\\.]+/tutorial/.*",
                 "^https?://docs\\.oracle\\.com/java(s|e)e/[\\d\\.]+/docs/technotes/guides/.*",
                 "^https?://docs\\.oracle\\.com/cd/(B|E)\\d{5}(_|-)01/.*",
                 "^https?://docs\\.oracle\\.com/javadb/[\\d\\.]+/ref/.*",
                 "^https?://docs\\.oracle\\.com/java(s|e)e/",
-                "^https?://docs\\.oracle\\.com/database/.*"
+                "^https?://docs\\.oracle\\.com/database/.*",
+                "^https?://java\\.sun\\.com/developer/technicalArticles.*",
+                "^https?://java\\.sun\\.com/docs/books.*"
+
         });
     }
 }
