dfc3418235bc8c1b5c53288477f1241932b7753b
prmr <prmr@gmx.com>
2018-05-23 18:20:38 -0400

    #1 Finish the progress checker

--- src/org/sotorrent/condor/CheckProgress.java
+++ src/org/sotorrent/condor/CheckProgress.java
@@ -11,20 +11,19 @@ import org.apache.commons.csv.QuoteMode;
 public class CheckProgress 
 {
 	private static final String COMMENTS = "data-collection/output/CommentLinks.csv";
+	private static final String POSTS = "data-collection/output/PostLinks.csv";
 	
-	 private static final CSVFormat csvFormatCommentLink, csvFormatClassifiedCommentLink;
+	 private static final CSVFormat csvFormatClassifiedCommentLink, csvFormatClassifiedPostLink;
 	    static {
-	        // configure CSV format for comment links
-	        csvFormatCommentLink = CSVFormat.DEFAULT
-	                .withHeader("PostId", "PostTypeId", "CommentId", "Protocol", "RootDomain", "CompleteDomain", "Path", "Url" , "MatchedDeveloperResource")
+	       csvFormatClassifiedCommentLink = CSVFormat.DEFAULT
+	                .withHeader("PostId", "PostTypeId", "CommentId", "Protocol", "RootDomain", "CompleteDomain", "Path", "Url", "MatchedDeveloperResource")
 	                .withDelimiter(',')
 	                .withQuote('"')
 	                .withQuoteMode(QuoteMode.MINIMAL)
-	                .withEscape('\\')
-	                .withFirstRecordAsHeader();
-	        // configure CSV format for classified comment links
-	        csvFormatClassifiedCommentLink = CSVFormat.DEFAULT
-	                .withHeader("PostId", "PostTypeId", "CommentId", "Protocol", "RootDomain", "CompleteDomain", "Path", "Url", "MatchedDeveloperResource")
+	                .withEscape('\\');
+	       
+	       csvFormatClassifiedPostLink = CSVFormat.DEFAULT
+	                .withHeader("PostId", "PostTypeId", "PostHistoryId", "Protocol", "RootDomain", "CompleteDomain", "Path", "Url", "MatchedDeveloperResource")
 	                .withDelimiter(',')
 	                .withQuote('"')
 	                .withQuoteMode(QuoteMode.MINIMAL)
@@ -35,12 +34,11 @@ public class CheckProgress
 	{
 		int total = 0;
 		int matched = 0;
-		try(CSVParser csvParser = new CSVParser(new FileReader(COMMENTS), csvFormatCommentLink ))
+		try(CSVParser csvParser = new CSVParser(new FileReader(COMMENTS), csvFormatClassifiedCommentLink ))
 		{
 			for (CSVRecord currentRecord : csvParser) 
 			{
 				total++;
-                String url = currentRecord.get("Url");
                 String resource = currentRecord.get("MatchedDeveloperResource");
                 if( !(resource.length() == 0 || resource.equals("NotMatched")))
                 {
@@ -51,6 +49,21 @@ public class CheckProgress
 		{
             e.printStackTrace();
         }
-		System.out.println(String.format("Matched %d out of %d (%.0f%%) comment links", matched, total, (double)matched*100/(double)total));
+		try(CSVParser csvParser = new CSVParser(new FileReader(COMMENTS), csvFormatClassifiedCommentLink ))
+		{
+			for (CSVRecord currentRecord : csvParser) 
+			{
+				total++;
+                String resource = currentRecord.get("MatchedDeveloperResource");
+                if( !(resource.length() == 0 || resource.equals("NotMatched")))
+                {
+                	matched++;
+                }
+            }
+        } catch(IOException e) 
+		{
+            e.printStackTrace();
+        }
+		System.out.println(String.format("Matched %d out of %d (%.0f%%) total links (posts + comments)", matched, total, (double)matched*100/(double)total));
 	}
 }
