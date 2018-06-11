3430816bc9cdc3251e8306893e364cfb0c07d670
Sebastian Baltes <research@sbaltes.com>
2018-06-11 16:55:19 +0200

    Fix Java API vs. Java Reference regex, update date

--- src/org/sotorrent/condor/resources/JavaReference.java
+++ src/org/sotorrent/condor/resources/JavaReference.java
@@ -13,17 +13,17 @@ public class JavaReference extends DeveloperResource
         createResourcePattern(new String[]{
                 "^https?://(docs|download)\\.oracle\\.com/java(s|e)e/tutorial.*",
                 "^https?://(docs|download)\\.oracle\\.com/java(s|e)e/[\\d\\.,]+/tutorial.*",
-                "^https?://(docs|download)\\.oracle\\.com/java(s|e)e/[\\d\\.,]+/docs.*",
-                "^https?://docs\\.oracle\\.com/cd/(B|E)\\d{5}(_|-)01.*",
+                "^https?://(docs|download)\\.oracle\\.com/java(s|e)e/[\\d\\.,]+/docs(?!\\/api).*",
+                "^https?://docs\\.oracle\\.com/cd/(B|E)\\d{5}(_|-)01(?!.*\\/api).*",
                 "^https?://docs\\.oracle\\.com/javadb/[\\d\\.,]+/ref.*",
-                "^https?://docs\\.oracle\\.com/java(s|e)e.*",
+                "^https?://docs\\.oracle\\.com/java(s|e)e(?!.*\\/api).*",
                 "^https?://docs\\.oracle\\.com/database.*",
                 "^https?://java\\.sun\\.com/developer/technicalArticles.*",
                 "^https?://java\\.sun\\.com/docs/books.*",
-                "^https?://java\\.sun\\.com/(j2se|javase)/[\\d\\.,]+/docs.*",
-                "^https?://java\\.sun\\.com/products/.+/(docs|articles).*",
-                "^https?://(www\\.)?oracle\\.com(/technetwork)?/(articles|java)?.*",
-                "^https?://download\\.oracle\\.com/(otn)?(docs|-pub).*",
+                "^https?://java\\.sun\\.com/(j2se|javase)/[\\d\\.,]+/docs(?!\\/api).*",
+                "^https?://java\\.sun\\.com/products/.+/(docs|articles)(?!.*\\/api).*",
+                "^https?://(www\\.)?oracle\\.com(/technetwork)?/(articles|java)?(?!.*\\/api).*",
+                "^https?://download\\.oracle\\.com/(otn)?(docs|-pub)(?!.*\\/api).*",
                 "^https?://blogs\\.oracle\\.com.*"
         });
     }
